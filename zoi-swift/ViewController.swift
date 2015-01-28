57136cbad81a77155558066e88feab5593606942
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-01-29 00:25:55 +0900

    Initial Commit

---
+++ zoi-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zoi-swift
+//
+//  Created by Tomohiro Nishimura on 2015/01/29.
+//  Copyright (c) 2015年 Tomohiro Nishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
